0357e29251353c878c0b7b9077291849b4dc18bd
divyachandrapandi <divya4shivalaya@gmail.com>
2025-09-29 12:49:45 +0530

    Added RTM Generation

--- routes/v1/rtmRouter.js
+++ routes/v1/rtmRouter.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { rtmController } from '../../controller/rtmContoller.js';
-
 export const rtmRouter = express.Router();
 
-rtmRouter.post('/generate-pdf', rtmController.generatePDF);
+// rtmRouter.post('/generate-pdf', rtmController.generatePDF);
+rtmRouter.post('/generate-rtm', rtmController.generateRTM);
+// rtmRouter.get('/files/:fileName', rtmController.fetchRTMFile);
+
+rtmRouter.get('/openai/health', rtmController.healthCheck);
+
 rtmRouter.get('/test', (req, res) => {
 
     res.send({ message: 'reached login from 5001' });
